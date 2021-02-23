6a56356aae3c120f6d95c50e78a3533e0a62935a
MrWatermelon99 <79535832+MrWatermelon99@users.noreply.github.com>
2021-02-23 15:34:06 -0500

    Update consoleProcessor.js

--- docs/DrawApp/functions/consoleProcessor.js
+++ docs/DrawApp/functions/consoleProcessor.js
@@ -4,9 +4,19 @@ var consoleProc = (function(){
   
   var publicCalls = {};
   
-  var publicCalls.log = function(mess, cont, size){
+  var publicCalls.log = function(mess, cont, size, color){
     
+    var color = color ? color : "#A6A6A6";
     
+    var p = document.createElement("p");
+    p.innterHTML = mess;
+    p.style.color = color;
+    p.style.fontSize = size + "px";
+    
+    cont = document.getElementById(cont);
+    cont.appendChild(p);
+    
+    cont.scrollTop = cont.scrollHeight;
     
   }
   
