19a9dbd56abaebae897af959ca4f23effd9dde29
MrWatermelon99 <79535832+MrWatermelon99@users.noreply.github.com>
2021-02-23 16:51:35 -0500

    Update main.js

--- docs/DrawApp/main.js
+++ docs/DrawApp/main.js
@@ -1,8 +1,11 @@
 // import functions from the files in /functions
-import colors from './functions/colors.js';
+import * as colors from 'functions/colors.js';
+import * as cons from 'functions/cons.js';
 
 // canvas code
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
 document.getElementById("b").innerHTML = colors.Hex2Array("#FFFFFF");
+
+cons.log("hello");
