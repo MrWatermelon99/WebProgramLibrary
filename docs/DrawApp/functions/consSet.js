1cacb5dd5f78f6cd4c3a313296ead29135d0c934
MrWatermelon99 <79535832+MrWatermelon99@users.noreply.github.com>
2021-02-23 16:47:52 -0500

    Create consSet.js

---
+++ docs/DrawApp/functions/consSet.js
@@ -0,0 +1,15 @@
+/*
+size: size of text in the console
+color: colors of the messages in this order:
+log
+warn
+error
+*/
+var consSet = {
+  "size": 16,
+  "color": [
+    "#C9C9C9",
+    "#FFA200",
+    "#F71D00"
+  ]
+};
