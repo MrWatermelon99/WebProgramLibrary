02644aab5d78947a811858a9a91bdb47e90e9096
MrWatermelon99 <79535832+MrWatermelon99@users.noreply.github.com>
2021-02-23 15:24:07 -0500

    Update consoleProcessor.js

--- docs/DrawApp/functions/consoleProcessor.js
+++ docs/DrawApp/functions/consoleProcessor.js
@@ -1,7 +1,13 @@
 export var consoleProc;
 
 var consoleProc = (function(){
-
-
-
+  
+  var publicCalls = {};
+  
+  var publicCalls.log = function(mess, cont, size){
+    
+    
+    
+  }
+  
 })();
