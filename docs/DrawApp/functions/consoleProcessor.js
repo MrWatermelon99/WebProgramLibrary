743acd5de4e1a4e452ecbfd73de209375bbb835b
MrWatermelon99 <79535832+MrWatermelon99@users.noreply.github.com>
2021-02-23 15:36:20 -0500

    Update consoleProcessor.js

--- docs/DrawApp/functions/consoleProcessor.js
+++ docs/DrawApp/functions/consoleProcessor.js
@@ -1,23 +1,17 @@
-export var consoleProc;
+export let log;
 
-var consoleProc = (function(){
+var log = function(mess, cont, size, color){
   
-  var publicCalls = {};
+  var color = color ? color : "#A6A6A6";
   
-  var publicCalls.log = function(mess, cont, size, color){
-    
-    var color = color ? color : "#A6A6A6";
-    
-    var p = document.createElement("p");
-    p.innterHTML = mess;
-    p.style.color = color;
-    p.style.fontSize = size + "px";
-    
-    cont = document.getElementById(cont);
-    cont.appendChild(p);
-    
-    cont.scrollTop = cont.scrollHeight;
-    
-  }
+  var p = document.createElement("p");
+  p.innterHTML = mess;
+  p.style.color = color;
+  p.style.fontSize = size + "px";
   
-})();
+  cont = document.getElementById(cont);
+  cont.appendChild(p);
+  
+  cont.scrollTop = cont.scrollHeight;
+  
+}
